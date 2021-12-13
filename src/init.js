3a3840ee1c23354c94270a9a5c7c7f1810ac717a
chanooda <hanrhfqkq@gmail.com>
2021-12-13 15:11:35 +0900

    build script

--- src/init.js
+++ src/init.js
@@ -5,6 +5,6 @@ import app from "./server";
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () =>
+app.listen(PORT, () =>
   console.log(`Server listening on port http://localhost:${PORT}`)
 );
