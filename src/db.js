ede795cbf46dc73955c18f6a67fa3aa8c3861b55
chanooda <hanrhfqkq@gmail.com>
2021-12-13 17:16:30 +0900

    build script

--- src/db.js
+++ src/db.js
@@ -4,8 +4,6 @@ console.log(process.env.DB_URL);
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
